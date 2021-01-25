452335cd64c13f9ab83b3b294cd1f820a5474da6
Pavithra <pavithra.sekar@bpmlinks.com>
2021-01-25 13:41:45 +0530

    Scatter plot for hierarchy cluster

---
+++ hierarchyCluster.py
@@ -0,0 +1,32 @@
+import numpy as nm
+import matplotlib.pyplot as mtp
+import pandas as pd
+dataset = pd.read_csv('resume_dataset.csv');
+print("dd",dataset)
+x = dataset.iloc[:, [0, 7609]].values
+
+import scipy.cluster.hierarchy as shc
+dendro = shc.dendrogram(shc.linkage(x, method="ward"))
+mtp.title("Dendrogram Plot")
+mtp.ylabel("Euclidean Distances")
+mtp.xlabel("Resumes")
+mtp.show()
+#training the hierarchical model on dataset
+
+
+from sklearn.cluster import AgglomerativeClustering
+hc= AgglomerativeClustering(n_clusters=5, affinity='euclidean', linkage='ward')
+y_pred= hc.fit_predict(x)
+print("predict",y_pred)
+import matplotlib.pyplot as mtp
+#visulaizing the clusters
+mtp.scatter(x[y_pred == 0, 0], x[y_pred == 0, 1], s = 100, c = 'blue', label = 'Cluster 1')
+mtp.scatter(x[y_pred == 1, 0], x[y_pred == 1, 1], s = 100, c = 'green', label = 'Cluster 2')
+mtp.scatter(x[y_pred== 2, 0], x[y_pred == 2, 1], s = 100, c = 'red', label = 'Cluster 3')
+mtp.scatter(x[y_pred == 3, 0], x[y_pred == 3, 1], s = 100, c = 'cyan', label = 'Cluster 4')
+mtp.scatter(x[y_pred == 4, 0], x[y_pred == 4, 1], s = 100, c = 'magenta', label = 'Cluster 5')
+mtp.title('Clusters of resumes')
+mtp.xlabel('weights')
+mtp.ylabel('scores')
+mtp.legend()
+mtp.show()
