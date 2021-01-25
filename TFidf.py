5bd019a35f8b090a9787fb454eaed554fed2c416
Pavithra <pavithra.sekar@bpmlinks.com>
2021-01-25 13:44:02 +0530

    generate csv file for cluster hierarchy

--- TFidf.py
+++ TFidf.py
@@ -1,9 +1,11 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
 import main as main
-
+import pandas as pd
 
 vectorizer = TfidfVectorizer()
 vectors = vectorizer.fit_transform(main.documents)
 feature_names = vectorizer.get_feature_names()
 dense = vectors.todense()
 denselist = dense.tolist()
+df = pd.DataFrame(denselist, columns=feature_names)
+df.to_csv('resume_dataset.csv')
